a2cedd19de63cda0cf0c537d6ce597a7ad91e791
kin7274 <kms6742@naver.com>
2018-10-05 02:33:32 +0900

    핀 설정 / 땜하고 시작하자
    핀 설정 / 땜하고 시작하자

--- Smart_Mountain_climbing_sticks/Smart_Mountain_climbing_sticks.ino
+++ Smart_Mountain_climbing_sticks/Smart_Mountain_climbing_sticks.ino
@@ -1,17 +1,26 @@
 // 등산스틱 아두이노 소스
-// 최종 수정 : 18.10.04
+
 // monochrome oled
-// Vin : 5V
+// Vin : 5V   // GND
 // DATA : D9
 // CLK : D10
 // DC : D11
 // CS : D12
 // RST : D13
 
+// ISD1820 녹음음성모듈(MAX 10s)
+// REC : 클릭 시 녹음
+// PLAYE : 클릭 시 재생
+// PLAYL : 클릭하는 동안만 녹음 재생
+// VCC, GND
+// PLAYE : D7
+
+// GPS모듈
+// RX D5
+// TX D6
+
 void setup() {
-  
 }
 
 void loop() {
-  
 }
